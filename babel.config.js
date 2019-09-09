a32f49958b7f3f7824def4752555243b3300df29
moriyak <keita.moriya@gmail.com>
2019-09-09 10:34:15 +0900

    fix setting

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { ie: 11 }, useBuiltIns: "usage" }],
+    [
+      "@babel/preset-env",
+      { targets: { ie: 11 }, useBuiltIns: "usage", corejs: 3 }
+    ],
     "@babel/preset-typescript"
   ]
 }
